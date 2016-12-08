4812540680240c94e4146b6db162316a9a359b59
Oly <hoit@bk.ru>
2016-12-08 19:53:31 +0300

    Update pop.cpp

--- tests/pop.cpp
+++ tests/pop.cpp
@@ -1,26 +1,71 @@
-#include "catch.hpp"
 #include <stack.hpp>
+#include "catch.hpp"
 
-SCENARIO ("if stack is empty")
+SCENARIO("if stack is empty")
 {
-   GIVEN("")
-   {      
-     stack<int> stack1;
-     stack1.push(1);
-     stack1.push(2);
-     stack1.push(3);
-     size_t count = stack1.count();
-      
-     WHEN("ok")
-       {  
-          stack1.pop();
+    GIVEN("empty stack")
+    {
+        stack<int> stack1;
        
-           THEN("")
-           {
-                REQUIRE(stack1.count() == count-1);
-           }
-       }
-   }
+        WHEN("")
+        {
+            stack1.pop();
+           
+            THEN("count must be 0")
+            {
+                REQUIRE(stack1.count() == 0);
+            }
+        }
+    }
 }
 
+SCENARIO("if stack isn't empty v1")
+{
+    GIVEN("stack")
+    {
+        stack<int> stack1;
+        st.push_back(1);
+        st.push_back(2);
+        st.push_back(3);
+        st.push_back(4);
+        st.push_back(5);
 
+        stack<int> stack2;
+        stack2.push_back(1);
+        stack2.push_back(2);
+        stack2.push_back(3);
+        stack2.push_back(4);
+        
+       WHEN("try to pop")
+        {
+            stack1.pop();
+            
+           THEN("stack 1 == stack 2")
+            {
+                REQUIRE(stack1 == stack2);
+            }
+        }
+    }
+}
+
+SCENARIO("if stack isn't empty v2")
+{
+    GIVEN("stack")
+    {
+        stack<int> stack1;
+        stack1.push(1);
+        stack1.push(2);
+        stack1.push(3);
+        stack1.push(4);
+        stack1.push(5);
+       
+        WHEN("try to pop")
+        {
+            stack1.pop();
+            THEN("count == 4")
+            {
+                REQUIRE(stack1.count() == 4);
+            }
+        }
+    }
+}
