1040fcb8cd84247764e764051de1de0ad667b1ff
Oly <hoit@bk.ru>
2016-12-08 09:50:08 +0300

    Update count.cpp

--- tests/count.cpp
+++ tests/count.cpp
@@ -1,1 +1,35 @@
+#include "catch.hpp"
+#include <stack.hpp>
 
+SCENARIO ("if stack is empty")
+{
+   GIVEN("")
+   {      
+     stack<int> stackf;
+           WHEN("ok")
+       {  
+           THEN("")
+           {
+                REQUIRE(stackf.count() = 0);
+           }
+       }
+   }
+}
+
+SCENARIO ("if stack isn'tempty")
+{
+   GIVEN("")
+   {      
+          stack<int> stacks;
+          stacks.push(1);  
+          stacks.push(2);
+          stacks.push(3);
+           WHEN("ok")
+       {  
+           THEN("")
+           {
+                REQUIRE(stacks.count() != 0);
+           }
+       }
+   }
+}
