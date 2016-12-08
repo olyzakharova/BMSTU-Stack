24608d6f0acd40c732a8de4366ca6581e72d065c
Oly <hoit@bk.ru>
2016-12-08 10:12:46 +0300

    Update push.cpp

--- tests/push.cpp
+++ tests/push.cpp
@@ -1,1 +1,24 @@
+#include "catch.hpp"
+#include <stack.hpp>
 
+SCENARIO ("")
+{
+   GIVEN("")
+   {      
+     stack<int> stack1;
+     stack1.push(1);
+     stack1.push(2);
+     stack1.push(3);
+     auto count = stack1.count();
+      
+     WHEN("ok")
+       {  
+          stack1.push(12);
+       
+           THEN("")
+           {
+                REQUIRE(stack1.count() == count+1);
+           }
+       }
+   }
+}
