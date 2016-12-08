a6c55fd70f56487e6b8032da9f94f6efc4734616
Oly <hoit@bk.ru>
2016-12-08 10:09:28 +0300

    Update pop.cpp

--- tests/pop.cpp
+++ tests/pop.cpp
@@ -1,1 +1,26 @@
+#include "catch.hpp"
+#include <stack.hpp>
+
+SCENARIO ("if stack is empty")
+{
+   GIVEN("")
+   {      
+     stack<int> stack1;
+     stack1.push(1);
+     stack1.push(2);
+     stack1.push(3);
+     auto count = stack1.count();
+      
+     WHEN("ok")
+       {  
+          stack1.pop();
+       
+           THEN("")
+           {
+                REQUIRE(stack1.count == count-1);
+           }
+       }
+   }
+}
+
 
