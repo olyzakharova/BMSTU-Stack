1dc21aadc7ea60dad2fe03ea7bcefd56620b7b81
Oly <hoit@bk.ru>
2016-12-08 20:12:59 +0300

    Update push.cpp

--- tests/push.cpp
+++ tests/push.cpp
@@ -1,24 +1,58 @@
 #include "catch.hpp"
 #include <stack.hpp>
 
-SCENARIO ("")
+SCENARIO("if stack is empty")
 {
-   GIVEN("")
-   {      
-     stack<int> stack1;
-     stack1.push(1);
-     stack1.push(2);
-     stack1.push(3);
-     auto count = stack1.count();
-      
-     WHEN("ok")
-       {  
-          stack1.push(12);
+    GIVEN("empty stack")
+    {
+        stack<int> stack1;
        
-           THEN("")
-           {
-                REQUIRE(stack1.count() == count+1);
-           }
-       }
-   }
+        WHEN("try to push ")
+        {
+            stack1.push(1);
+           
+            THEN(" count == 1")
+            {
+                REQUIRE(stack1.count() == 1);
+            }
+        }
+    }
+}
+
+SCENARIO("if stack isn't empty (but isn't full)")
+{
+    GIVEN("stack")
+    {
+        stack<int> stack1;
+        stack1.push(2);
+        WHEN("try to push new element")
+        {
+            stack1.push(4);
+            THEN("")
+            {
+                REQUIRE(stack.count() == 2);
+            }
+        }
+    }
+}
+SCENARIO("if full stack")
+{
+    GIVEN("full stack")
+    {
+        stack<int> stack1;
+        stack1.push(1);
+        stack1.push(2);
+        stack1.push(3);
+        stack1.push(4);
+       
+        WHEN("try to push")
+        {
+            stack1.push(5);
+           
+            THEN("count == 5")
+            {
+                REQUIRE(stack1.count() == 5);
+            }
+        }
+    }
 }
