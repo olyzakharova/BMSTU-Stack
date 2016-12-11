1eb6c7b35a19901b1b62f88c45955302d1a72aca
Oly <hoit@bk.ru>
2016-12-11 12:07:44 +0300

    stage 2

---
+++ tests/top.cpp
@@ -0,0 +1,42 @@
+#include <slack.hpp>
+#include "catch.hpp"
+
+SCENARIO("if stack is empty (top-test)")
+{
+    GIVEN("empty stack")
+    {
+        stack<int> stack1;
+        
+        WHEN("try")
+        {
+            THEN("")
+            {
+                REQUIRE(stack1.top());
+            }
+            
+        }
+        
+    }
+    
+}
+
+
+SCENARIO("if stack isn't empty (top-test)")
+{
+    GIVEN(" stack")
+    {
+        stack<int> stack1;
+        stack1.push(1);
+        
+        WHEN("try")
+        {
+            THEN("")
+            {
+                REQUIRE(stack1.top());
+            }
+            
+        }
+        
+    }
+    
+}
