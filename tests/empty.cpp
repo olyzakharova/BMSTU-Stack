8b1f069d9ccc7c008f9d040edbe377439d085e8d
Oly <hoit@bk.ru>
2016-12-11 11:55:25 +0300

    stage2

---
+++ tests/empty.cpp
@@ -0,0 +1,50 @@
+include <slack.hpp>
+include "catch.hpp"
+
+#include "catch.hpp"
+#include <stack.hpp>
+
+SCENARIO("if stack is empty (must return true) ")
+{
+    GIVEN("stack")
+    {
+       stack<int> stack1;
+        
+        WHEN("try")
+        {
+            THEN("true")
+            {
+            
+              REQUIRE(stack1.empty());
+              
+            }
+            
+        }
+        
+    }
+    
+}
+
+
+
+SCENARIO("if stack isn't empty (must return false)")
+{
+    GIVEN("non-empty stack")
+    {
+        stack<int> stack1;
+        stack1.push(2);
+       
+       WHEN("try")
+         {
+            THEN("return false")
+            {
+            
+                REQUIRE(stack1.empty());
+                
+            }
+            
+        }
+        
+    }
+    
+}
